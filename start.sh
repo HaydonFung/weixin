bc872a7767cd5b5f8edc7c7a0193d2a2bd965612
Hasee <Hasee@NaNi>
2019-04-27 19:50:24 +0800

    dd

--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
 # !/bin/bash
 # 每次总是重启所有程序的脚本
 # 在文件保存在父项目里面，扩展名为.sh
-# 可以在服务器里面执行chmod  u+x  *.sh的方式增加可执行权限
 mvn install
 cd test_wx
 mvn spring-boot:start
 cd ../subscribe
+mvn spring-boot:start
+cd ../unsubscribe
 mvn spring-boot:start
--- stop.sh
+++ stop.sh
@@ -3,3 +3,5 @@ cd test_wx
 mvn spring-boot:stop
 cd ../subscribe
 mvn spring-boot:stop
+cd ../unsubscribe
+mvn spring-boot:stop
