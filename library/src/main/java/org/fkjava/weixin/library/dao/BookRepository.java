12cf61100bc6f33140691bddeef70621bcd7471e
Hasee <Hasee@NaNi>
2019-06-26 10:15:59 +0800

    图书

---
+++ library/src/main/java/org/fkjava/weixin/library/dao/BookRepository.java
@@ -0,0 +1,14 @@
+package org.fkjava.weixin.library.dao;
+
+import org.fkjava.weixin.library.domain.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, String>{
+
+	Page<Book> findByNameContaining(String keyword, Pageable pageable);
+
+}
