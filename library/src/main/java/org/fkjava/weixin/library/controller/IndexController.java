a16deff78ded9c18cc8d4e38ec355afd94136dd2
Hasee <Hasee@NaNi>
2019-07-04 23:04:22 +0800

    test

--- library/src/main/java/org/fkjava/weixin/library/controller/IndexController.java
+++ library/src/main/java/org/fkjava/weixin/library/controller/IndexController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/kemao_3_fz_library_index")
+@RequestMapping("/kemao_3_fz/library/index")
 public class IndexController {
 
 	@Autowired
